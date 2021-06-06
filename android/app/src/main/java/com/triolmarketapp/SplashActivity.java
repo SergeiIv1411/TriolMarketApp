03465e70373d10ec738e3d9f6a56fcbd4937b040
SergeiIv1411 <s9153104109@gmail.com>
2021-06-06 18:44:33 +0300

    add app icon for android/ios

--- android/app/src/main/java/com/triolmarketapp/SplashActivity.java
+++ android/app/src/main/java/com/triolmarketapp/SplashActivity.java
@@ -1,4 +1,17 @@
 package com.triolmarketapp;
 
-public class SplashActivity {
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity  extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
 }
